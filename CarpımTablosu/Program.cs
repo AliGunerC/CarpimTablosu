6288c24748eac6051502a256700325bb86137ecf
Ali Guner <Ali Guner@DESKTOP-334HR1I>
2023-07-01 15:35:15 +0300

    Proje dosyası ekle.

---
+++ CarpımTablosu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpımTablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 1; j < 11; j++)
+                {
+                    Console.WriteLine($" {j} * {i} = {i*j} ");
+                    if (j == a)
+                    {
+                        Console.Write("");
+                        
+                    }
+                    
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
